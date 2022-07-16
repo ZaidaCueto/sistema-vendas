be512e3a638d6f2c7c709d7043f720ccd50d97cd
zaida cueto <zcueto58@gmail.com>
2022-07-15 23:53:46 -0300

    Other component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg';
+import "./styles.css";
+
+
+function Header() {
+    return(
+        <div className="logo-container">
+        <img src={logo} alt="logo" />
+        <h1>Sistema de controle de vendas</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://www.linkedin.com/in/zaidacuetoa">  Zaida Cueto</a>
+        </p>
+    </div>
+     
+     )
+   
+   }
+   
+   export default Header
+   
