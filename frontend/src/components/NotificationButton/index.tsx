f565206dda3c4bcc2417aed6c7f42dcc2f92290f
zaida cueto <zcueto58@gmail.com>
2022-07-15 23:06:15 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,18 @@
+import icon from '../../assets/img/notification-icon.png';
+import "./styles.css";
+
+function NotificationButton() {
+    return(
+        <>
+        <div className='red-button'>
+            <img src={icon} alt="" />
+        </div>
+      
+        </>
+     
+     )
+   
+   }
+   
+   export default NotificationButton
+   
