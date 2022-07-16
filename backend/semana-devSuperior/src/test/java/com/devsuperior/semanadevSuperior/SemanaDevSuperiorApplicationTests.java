64a741c707cf737600ac5041f667fcc81ffd48a7
zaida cueto <zcueto58@gmail.com>
2022-07-15 22:02:26 -0300

    Project created

---
+++ backend/semana-devSuperior/src/test/java/com/devsuperior/semanadevSuperior/SemanaDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.semanadevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanaDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
