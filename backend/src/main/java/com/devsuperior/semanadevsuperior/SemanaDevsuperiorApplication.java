67f1dba03fd08f6a0e48c14400aad9f0c9da158b
zaida cueto <zcueto58@gmail.com>
2022-07-16 21:38:30 -0300

    Security config

---
+++ backend/src/main/java/com/devsuperior/semanadevsuperior/SemanaDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.semanadevsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaDevsuperiorApplication.class, args);
+	}
+
+}
