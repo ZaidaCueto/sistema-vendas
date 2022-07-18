36cc6b6fcb9c7126995e4e89df43fb99b3df0d47
zaida cueto <zcueto58@gmail.com>
2022-07-17 22:17:13 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,24 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.png';
+import { BASE_URL } from '../../utils/request';
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number) {
+axios (`${BASE_URL}/sales/52/notification`)
+.then(res => {
+console.log("sucesso");
+})
+}
+
+
+function NotificationButton({ saleId }: Props) {
     return(
         <>
-        <div className='red-button'>
+        <div className='red-button' onClick={()=> handleClick(saleId)}>
             <img src={icon} alt="" />
         </div>
       
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,7 +20,7 @@ function SalesCard() {
   useEffect(() => {
 const dmin = minDate.toISOString().slice(0, 10);
 const dmax= maxDate.toISOString().slice(0, 10);
-console.log(dmin);
+
 
     axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
       .then(res => {
@@ -78,7 +78,8 @@ console.log(dmin);
                 <td>{sale.amount.toFixed(2)}</td> 
                 <td>
                   <div className="red-btn-container">
-                    <NotificationButton />
+                    <NotificationButton 
+                    saleId={sale.id}/>
                   </div>
                 </td>
               </tr>
